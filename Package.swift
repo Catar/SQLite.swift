6c1369ad5bb6fac63584f7af20c549bd6108a222
Evgenii Mikhailin <evgeniy.catar@gmail.com>
2020-06-03 11:47:28 +0300

    swift tools 5.2

--- Package.swift
+++ Package.swift
@@ -1,13 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SQLite.swift",
     products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
-        .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
+        .target(name: "SQLite", 
+                dependencies: [
+                    .target(name: "SQLiteObjc"),
+                ]),
         .target(name: "SQLiteObjc"),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
+        .testTarget(name: "SQLiteTests",
+                    dependencies: [
+                        .target(name:"SQLite"),
+                    ],
+                    path: "Tests/SQLiteTests")
     ],
     swiftLanguageVersions: [4, 5]
 )
